c85748fe0022149050b66fde046a9a1bce63d8ab
Sam Kreter <samkreter@gmail.com>
2015-09-15 23:05:05 -0500

    added 'ng-token-auth' to app.js as imported module

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('sw', ['ngLodash', 'ui.router'])
+angular.module('sw', ['ngLodash', 'ui.router','ng-token-auth'])
     .run(function($rootScope, $state, API) {
         $rootScope.$state = $state;
         API.getAccount()
